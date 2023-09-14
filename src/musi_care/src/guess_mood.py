f6b6d4591dc00b287247ff641d07f84720400454
Jwaad Hussain <jwaad@hotmail.co.uk>
2023-09-14 04:51:57 +0100

    better docs, can now inherit pygame methods.

--- src/musi_care/src/guess_mood.py
+++ src/musi_care/src/guess_mood.py
@@ -32,9 +32,16 @@ from musicare_lib import General
 class Guess_The_Mood_Game():
     """ Class to generate and handle guess the mood game """
 
-    def __init__(self, reduce_screen=True, debug=False):
-        """Initialise and take user_id, user_id helps us save the data to the specific profiles"""
-        x = 145  # x pos of screen
+    def __init__(self, reduce_screen=True, debug=False, screen = None, my_pygame = None):
+        """
+        reduce_screen = bool, whether or not to reduce borders to account for linux toolbar
+        debug = bool, whether or not to display things like FPS and minor helpful tools.
+        screen = pygame_screen, should be a pygame window, can be used as param, to blit graphics onto a specific screen,
+            instead of making a new one
+        my_pygame = pygame lib, should be initiated pygame lib. Similar to screen, can be parameter, to use a pre init
+            pygame, instead of init our own
+        """
+        """x = 145  # x pos of screen
         y = 0  # y pos of screen
         os.environ['SDL_VIDEO_WINDOW_POS'] = '%d,%d' % (x, y)  # move screen to x and y pos
         self.previous_screen = ""  # used so we can go backwards a screen
@@ -53,15 +60,35 @@ class Guess_The_Mood_Game():
         self.cen_x = self.window_center[0]
         self.cen_y = self.window_center[1]
         self.window = pygame.display.set_mode((self.window_x, self.window_y))  # Create window and set size
+        """
+        x = 145  # x pos of screen
+        y = 0  # y pos of screen
+        os.environ['SDL_VIDEO_WINDOW_POS'] = '%d,%d' % (x, y)  # move screen to x and y pos
+
+        if my_pygame == None:
+            self.pygame = pygame
+            self.pygame.init()  # start py engine
+            self.pygame.freetype.init()
+        else:
+            self.pygame = my_pygame
+        res = pygame.display.Info()  # get our screen resolution
+        if screen == None:
+            if reduce_screen:
+                self.window_x = res.current_w - 150  # Width of window -150 to account for the linux toolbar
+            else:
+                self.window_x = res.current_w
+            self.window_y = res.current_h  # Height of window
+            self.window_center = (int(self.window_x / 2), int(self.window_y / 2))
+            self.cen_x = self.window_center[0]
+            self.cen_y = self.window_center[1]
+            self.window = pygame.display.set_mode((self.window_x, self.window_y))  # Create window and set size
+        else:
+            self.window = screen
+            self.window_center = (int(self.window.get_height() / 2), int(self.window.get_width() / 2))
         self.background_colour = (100, 100, 100)  # background grey by default
         self.pygame.display.set_caption("Guess The Mood!")  # Label window
         self.run = True
         self.quit = False  # Check to see if the game ended or it was quit
-        self.track_playing = False
-        self.previous_track_data = None
-        self.difficulty = "easy"  # Default difficulty to play
-        self.current_level = 1  # Default level to play
-        self.music_data = self.get_song_database()  # From save file load all of the level data
         self.music_filepath = "/game_assets/music/"  # relative path to songs # "/home/qtrobot/catkin_ws/src/musi_care/src/game_assets/music/"
         self.timer_manager = TimeFunctions()
         self.animation_manager = AnimationManager(self.pygame)
@@ -71,6 +98,11 @@ class Guess_The_Mood_Game():
         self.behaviours_manager = Behaviours(self.pygame, self.music_filepath)
         self.level_loader = StandardLevels(self.window, self.window_center, self.pygame, self.music_filepath)
         self.gm = General()  # gm = general methods
+        self.track_playing = False
+        self.previous_track_data = None
+        self.difficulty = "easy"  # Default difficulty to play
+        self.current_level = 1  # Default level to play
+        self.music_data = self.get_song_database()  # From save file load all of the level data
         # self.music_vol = 1 # change volume of laptop
         # self.qt_voice_vol
         # self.sound_manager.volume_change(self.music_vol) # Set a default volume
